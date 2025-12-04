f8204570199a50c92ef6587414c9e5041fa892e2
Christian Thiel <127086519+tchris15@users.noreply.github.com>
2025-12-04 14:16:31 +0100

    Initial commit

---
+++ src/main/java/net/chris/createshufflefilterfabric/CreateShuffleFilterFabricClient.java
@@ -0,0 +1,10 @@
+package net.chris.createshufflefilterfabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreateShuffleFilterFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/chris/createshufflefilterfabric/CreateShuffleFilterFabricDataGenerator.java
@@ -0,0 +1,11 @@
+package net.chris.createshufflefilterfabric;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CreateShuffleFilterFabricDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
